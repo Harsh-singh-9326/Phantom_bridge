8c599988ada3f7135a8bb7ba841d2df7282fdd0d
Harsh-singh-9326 <harshS9326@gmail.com>
2025-07-16 22:42:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-indigo-600">
+      <h1 className="text-white text-3xl font-bold">Hello from Tailwind + Vite!</h1>
+    </div>
+  );
+}
+
+export default App;
